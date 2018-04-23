4b0bdde87ed45c670eb2f28da249dd3329c58683
aspasia <aspasia.beneti@gmail.com>
2018-04-23 19:00:17 +0200

    Added "other" tests and added slow flag

--- test/social_wallet_api/test/precision.clj
+++ test/social_wallet_api/test/precision.clj
@@ -38,8 +38,7 @@
                     (facts "Check specific amounts" 
                            (fact "Check one Satochi (8 decimal)"
                                  (let [response (new-transaction-request (str Satoshi))
-                                       body (parse-body (:body response))
-                                       _ (log/info @h/blockchains)]
+                                       body (parse-body (:body response))]
                                    (:status response) => 200
                                    (:amount body) => Satoshi
                                    (:amount-text body) => (str Satoshi)
@@ -61,13 +60,13 @@
                                    (:error body) => "Negative values not allowed."
                                    (:amount-text body) => nil)))
                     
-                    (facts "Check different doubles"
+                    (facts "Check different doubles" :slow
                            (for-all
                             [rand-double (gen/double* {:min Satoshi
                                                        :max int16-fr8
                                                        :NaN? false
                                                        :infinite? false})]
-                            {:num-tests 100
+                            {:num-tests 200
                              :seed 1524497634230}
                             (fact "Generative tests"
                                   (let [amount (.toString (BigDecimal. rand-double))  
@@ -79,14 +78,12 @@
                                     (:amount-text (get-latest-transaction "test-1")) => amount)))
                            
 
-                           #_(fact "Check other inputs"
+                           (fact "Check other inputs" :slow
                                  (for-all
-                                  [other (gen/one-of [gen/string gen/boolean gen/uuid gen/byte ])]
-                                  #_{:num-tests 100
-                                     :max-size 20
-                                     #_:seed #_1510160943861}
+                                  [other (gen/one-of [gen/string gen/boolean gen/uuid])]
+                                  {:num-tests 200}
                                   (fact "A really large number with 16,8 digits"
-                                        (let [amount other 
+                                        (let [amount (log/spy (str other)) 
                                               response (h/app
                                                         (->
                                                          (mock/request :post "/wallet/v1/transactions/new")
@@ -99,7 +96,4 @@
                                               body (parse-body (:body response))]
                                           (:status response) => 400
                                           (class (:error body)) => String
-                                          (:error body) => "The amount is not valid."))))
-
-                           #_(fact "Check that the amount returned after the creation of a transanction in mongo is the same as the input one"
-                                 )))
+                                          (:error body) => truthy))))))
